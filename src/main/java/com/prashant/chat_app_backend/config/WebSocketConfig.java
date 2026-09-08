6bc4a5f5748fa579b6d7fd67a0e4b93a78ef2e00
puchakayaladinesh12-wq <puchakayaladinesh12@.in>
2026-09-08 18:20:50 +0530

    Allow Vercel frontend origin

--- src/main/java/com/prashant/chat_app_backend/config/WebSocketConfig.java
+++ src/main/java/com/prashant/chat_app_backend/config/WebSocketConfig.java
@@ -19,11 +19,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns(
-                        "http://localhost:5500",
-                        "http://127.0.0.1:5500"
-                )
-                .withSockJS();
+       registry.addEndpoint("/ws")
+    .setAllowedOriginPatterns(
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "https://connectly-chat-eta.vercel.app"
+    )
+    .withSockJS();
     }
 }
